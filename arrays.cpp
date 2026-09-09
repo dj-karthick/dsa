62b953200033ab787b6d251473d47a4e8c1ea35e
dj-karthick <karthickallancherry@gmail.com>
2026-09-09 20:47:07 +0530

    Finished the Largest, Smallest, Second Largest and Second Smallest

---
+++ arrays.cpp
@@ -0,0 +1,79 @@
+
+#include <climits>
+#include <iostream>  // For input and output
+#include <vector>           // For dynamic arrays
+#include <algorithm>        // For sorting, searching, etc.
+#include <set>              // For ordered sets
+#include <map>              // For hash maps
+#include <unordered_map>    // For unordered hash maps
+#include <queue>            // For priority queues and queues
+#include <stack>            // For stacks
+#include <limits>           // For numeric limits (e.g., infinity)
+#include <cmath>            // For mathematical operations
+
+using namespace std;
+
+int largest_element(vector<int>& arr, int n){
+    int largest = arr[0];
+    for(int i = 0; i < n; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    cout << "Largest Element is: " << largest;
+    return largest;
+}
+
+int second_largest(vector<int>& arr, int n){
+    int largest = arr[0];
+    int s_largest = -1;
+    for(int i = 1; i < n; i++){
+        if(arr[i] > largest){
+            s_largest = largest;
+            largest = arr[i];
+        }else if (arr[i] < largest &&  arr[i] > s_largest) {
+            s_largest = arr[i];
+        }
+    }
+    return s_largest;
+}
+
+int second_smallest(vector<int>& arr, int n){
+    int smallest = arr[0];
+    int s_smallest = INT_MAX;
+    for(int i = 1; i < n; i++){
+        if(arr[i] < smallest){
+            s_smallest = smallest;
+            smallest = arr[i];
+        }else if (arr[i] > smallest && arr[i] < s_smallest) {
+            s_smallest = arr[i];
+        }
+    }
+    return s_smallest;
+}
+
+vector<int> slargest_ssmallest(vector<int>& arr, int n){
+    int s_largest = second_largest(arr, n);
+    int s_smallest = second_smallest(arr, n);
+    cout << "Second Largest Number is: " << s_largest << endl;
+    cout << "Second Smallest Number is: " << s_smallest << endl;
+    return {s_largest, s_smallest};
+}
+
+int main() {
+    // Your code here
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    // largest_element(arr, n);
+    vector<int>small_large = slargest_ssmallest(arr, n);
+    for(auto it: small_large){
+        cout << it << endl;
+    }
+    
+    return 0;
+}
+
