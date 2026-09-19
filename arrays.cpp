0cf3471fad9190cba8992e59ec3c915f1852ae0f
dj-karthick <karthickallancherry@gmail.com>
2026-09-19 18:40:28 +0530

    Finished Union and Intersection Problem

--- arrays.cpp
+++ arrays.cpp
@@ -89,27 +89,38 @@ void left_rotate_one(vector<int>& arr, int n){
 }//Tc -> O(N)
 
 //BruteForce -> Tc-> O(n+d), Sc-> O(d)
-// void left_rotate_k(vector<int>& arr, int n, int k){
-//     if(k == 0 || k == n){
-//         return;
-//     }else if(k > n){
-//         k = k % n;
-//         if(k == 0 || k == n){
-//             return;
-//         }
-//     }
-//     vector<int> temp(k, 0);
+void brute_left_rotate_k(vector<int>& arr, int n, int k){
+    if(k == 0 || k == n){
+        return;
+    }else if(k > n){
+        k = k % n;
+        if(k == 0 || k == n){
+            return;
+        }
+    }
+    vector<int> temp(k, 0);
 
-//     for(int i = 0; i < k; i++){
-//         temp[i] = arr[i];
-//     }
-//     for(int i = k; i < n; i++){
-//         arr[i-k] = arr[i];
-//     }
-//     for(int i = n-k; i < n; i++){
-//         arr[i] = temp[i-(n-k)];
-//     }
-// }
+    for(int i = 0; i < k; i++){
+        temp[i] = arr[i];
+    }
+    for(int i = k; i < n; i++){
+        arr[i-k] = arr[i];
+    }
+    for(int i = n-k; i < n; i++){
+        arr[i] = temp[i-(n-k)];
+    }
+}//Tc-> O(n+d), O(d)
+
+//If You dont use Reverse algorithm.
+void reverse_arr(int arr[], int start, int end){
+    while(start < end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
 
 //Optimal Solution -> Tc -> O(2N), Sc -> O(1)
 void left_rotate_k(int arr[], int n, int k){
@@ -124,19 +135,147 @@ void left_rotate_k(int arr[], int n, int k){
     reverse(arr, arr+k);
     reverse(arr+k, arr+n);
     reverse(arr, arr+n);
-}
+}//Tc -> O(2n), Sc-> O(1)
+
+//Brute force solution.
+void brute_move_zero_end(vector<int>& arr, int n){
+    vector<int> temp;
+    for(int i = 0; i < n; i++){
+        if(arr[i] != 0){
+            temp.push_back(arr[i]);
+        }
+    }
+    for(int i = 0; i < temp.size(); i++){
+        arr[i] = temp[i];
+    }
+    for(int i = temp.size(); i < n; i++){
+        arr[i] = 0;
+    }
+}//Tc-> O(2n), Sc->O(n)
+
+void move_zero_end(vector<int>& arr, int n){
+    int j = -1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if(j == -1) return;
+    for(int i = j+1; i < n; i++){
+        if(arr[i] != 0){
+
+            arr[j] = arr[i];
+            arr[i] = 0;
+            j++;
+        }
+    }
+}//Tc-> O(n), Sc-> O(1)
+
+int linear_search(vector<int>& arr, int n, int num){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == num) return i;
+    }
+    return -1;
+}//Tc-> O(n), Sc-> O(1)
+
+vector<int> brute_union_sorted(vector<int>& arr1, vector<int>& arr2){
+    set<int> st;
+    for(int i = 0; i < arr1.size(); i++){
+        st.insert(arr1[i]);
+    }
+    for(int i = 0; i < arr2.size(); i++){
+        st.insert(arr2[i]);
+    }
+    vector<int> uni_arr;
+    for(auto it: st){
+        uni_arr.push_back(it);
+    }
+    return uni_arr;
+}//Tc->O(n1log n + n2log n) + O(n1+n2), Sc-> O(n1+n2) + O(n1+n2)
+
+//Optimal solution
+vector<int> union_sorted(vector<int>& arr1, vector<int>& arr2){
+    int i = 0;
+    int j = 0;
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    vector<int> union_arr;
+
+    while(i < n1 && j < n2){ 
+        if(arr1[i] <= arr2[j]){
+            if(union_arr.size() == 0 || union_arr.back() != arr1[i]){
+                union_arr.push_back(arr1[i]);
+            }
+            i++;
+        }
+        else{
+            if(union_arr.size() == 0 || union_arr.back() != arr2[j]){
+                union_arr.push_back(arr2[j]);
+            }
+            j++;
+        }
+    }
+
+    while(i < n1){
+        if(union_arr.back() != arr1[i]){
+            union_arr.push_back(arr1[i]);
+        }
+        i++;
+    }
+
+    while(j < n2){
+        if(union_arr.back() != arr2[j]){
+            union_arr.push_back(arr2[j]);
+        }
+        j++;
+    }
+    return union_arr;
+}//Tc-> O(n1 + n2), Sc-> O(n1 + n2)
+
+vector<int> brute_intersection_sorted(vector<int>& a, vector<int>& b){
+    vector<int> ans;
+    vector<int> vis(b.size(), 0);
+    for(int i = 0; i < a.size(); i++){
+        for(int j = 0; j < b.size(); j++){
+            if(a[i] == b[j] && vis[j] == 0){//For Marking Visited or Not
+                break;
+                ans.push_back(a[i]);
+                vis[j] = 1;
+            }
+            else if(b[j] > a[i]) break;//Because both arrays are already sorted when this block satisfies all the elements in the b[j] will be greater than a[i] so we break and moved the i pointer to next.
+        }
+    }
+    return ans;
+}//Tc-> O(n1 * n2), Sc-> O(n2)
+
+vector<int> intersection_sorted(vector<int>& a, vector<int>& b){
+    int i = 0;
+    int j = 0;
+    vector<int> ans;
+    while(i < a.size() && j < b.size()){
+        if(a[i] < b[j]) i++;
+        else if(a[i] > b[j]) j++; 
+        else{
+            ans.push_back(a[i]);
+            i++;
+            j++;
+        }
+    }
+    return ans; 
+}//Tc-> O(n1 + n2), Sc-> O(1)
 
 int main() {
     // Your code here
-    int n;
-    cin >> n;
-    int k;
-    cin >> k;
+    // int n;
+    // cin >> n;
+    // int k;
+    // cin >> k;
     // vector<int> arr(n, 0);
-    int arr[n];
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
+    // int arr[n];
+    // for(int i = 0; i < n; i++){
+    //     cin >> arr[i];
+    // }
     // largest_element(arr, n);
     // vector<int>small_large = s_largestS_smallest(arr, n);
     // if(is_sorted(arr, n)) cout << "Sorted";
@@ -144,8 +283,31 @@ int main() {
     // int i = remove_duplicate(arr, n);
     // cout << i << endl;
     // left_rotate_one(arr, n);
-    left_rotate_k(arr, n, k);
-    for(auto it: arr){
+    // left_rotate_k(arr, n, k);
+    // reverse_arr(arr, 0, n-1);
+    // brute_move_zero_end(arr, n);
+    // move_zero_end(arr, n);
+    // cout << "Index is :" << linear_search(arr, n, 4) << endl;
+    int n1;
+    cin >> n1;
+    vector<int> arr1(n1);
+    for(int i = 0; i < n1; i++){
+        cin >> arr1[i];
+    }
+
+    int n2;
+    cin >> n2;
+    vector<int> arr2(n2, 0);
+    for(int i = 0; i < n2; i++){
+        cin >> arr2[i];
+    }
+    
+
+    // vector<int> uni_arr = brute_union_sorted(arr1, arr2);
+    // vector<int> union_arr = union_sorted(arr1, arr2);
+    // vector<int> intersection_arr = brute_intersection_sorted(arr1, arr2);
+    vector<int> intersection_arr = intersection_sorted(arr1, arr2);
+    for(auto it: intersection_arr){
         cout << it << endl;
     }
     
